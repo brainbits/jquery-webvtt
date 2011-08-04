bd8e8c6a9af2f78ba17080d2d9c4c0a6132bf45f
Michael van Engelshoven <michael@van-engelshoven.de>
2011-08-04 15:27:08 +0200

    Fixed all the jsLint errors

--- jquery.webvtt.js
+++ jquery.webvtt.js
@@ -7,7 +7,9 @@
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://jquery.org/license
  */
-(function($, undefined) {
+(function ($) {
+
+    "use strict";
 
     /**
      * Some regular expressions for parsing
@@ -17,9 +19,34 @@
         WEBVTT    = /^\uFEFF?WEBVTT(?: .*)/;
 
     /**
+     * Converts a WebVTT timestamp into a floating number
+     */
+    function timestampToNumber(time) {
+
+        if (!TIMESTAMP.test(time)) {
+            throw "'" + time + "' doesn't match to the WebVTT timestamp pattern.";
+        }
+
+        var matches = TIMESTAMP.exec(time),
+            number  = matches[4] / 1000;
+
+        number += parseInt(matches[3], 10);
+
+        if (matches[2]) {
+            number += matches[2] * 60;
+        }
+
+        if (matches[1]) {
+            number += matches[1] * 60 * 60;
+        }
+
+        return number;
+    }
+    
+    /**
      * Parse the WebVTT source into a javascript array
      */
-    var parse = function(text) {
+    function parse(text) {
 
         var lines   = text.split(/(\n{2,})/),
             cues    = [],
@@ -29,7 +56,7 @@
         do {
 
             // If there is the optional WebVTT Header, omit first two lines
-            if(i === 0 && WEBVTT.test(lines[i])) {
+            if (i === 0 && WEBVTT.test(lines[i])) {
                 i += 2;
             }
 
@@ -44,7 +71,7 @@
                 from: timestampToNumber(matches[2]),
                 to: timestampToNumber(matches[3]),
                 payload: $('<p/>').text(matches[4])
-            })
+            });
 
             i += 2;
             
@@ -54,34 +81,9 @@
     }
 
     /**
-     * Converts a WebVTT timestamp into a floating number
-     */
-    var timestampToNumber = function(time) {
-
-        if(!TIMESTAMP.test(time)) {
-            throw "'" + time + "' doesn't match to the WebVTT timestamp pattern.";
-        }
-
-        var matches = TIMESTAMP.exec(time),
-            number  = matches[4]/1000;
-
-        number += parseInt(matches[3]);
-        
-        if (matches[2]) {
-            number += matches[2] * 60;
-        }
-
-        if (matches[1]) {
-            number += matches[1] * 60 * 60;
-        }
-
-        return number;
-    }
-
-    /**
      * The global jquery function
      */
-    $.webVtt = function(source, time) {
+    $.webVtt = function (source, time) {
 
         var element = $(source),
             matches = $([]);
@@ -94,7 +96,7 @@
             time = timestampToNumber(time);
         }
 
-        $.each(element.data('webvtt'), function(index, cue) {
+        $.each(element.data('webvtt'), function (index, cue) {
             if (cue.from <= time && cue.to > time) {
                 matches = matches.add(cue.payload);
             }
@@ -107,9 +109,9 @@
      * Extends the jquery functions for concatenation
      */
     $.fn.extend({
-        webVtt: function(time) {
+        webVtt: function (time) {
             return $.webVtt(this[0], time);
         }
     });
 
-}) (jQuery);
+}(jQuery));
