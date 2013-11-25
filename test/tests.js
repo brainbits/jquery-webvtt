6cd6137ebe7cb393c436888da8d7620b4d6f479e
Michael van Engelshoven <michael@van-engelshoven.de>
2013-11-25 10:42:18 +0100

    Added test case to work with plain webvtt string in IE8

--- test/tests.js
+++ test/tests.js
@@ -21,3 +21,15 @@ test("Writes no text to html if time is after last line", function() {
 
     equal(output.children('p').length, 0, 'No paragraph added');
 });
+
+test("Loading content from a string", function() {
+    var output = $('#qunit-fixture'),
+        ajaxResponse = $('#source').html(),
+        element = $('<script type="text/vtt">' + ajaxResponse + '</script>');
+
+    equal(typeof ajaxResponse, "string", "For this test we expecting a string as source");
+
+    element.webVtt('00:05.501').appendTo(output);
+
+    equal(output.children('p').text(), 'Text number two', "Text is correct");
+})
